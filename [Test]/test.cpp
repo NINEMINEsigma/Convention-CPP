4ab5c2378ae66d2be7554d8ebaa0f1f41f885d7d
ninemine <1371605831@qq.com>
2025-07-29 11:01:54 +0800

    更新关于nlohmann的依赖

--- [Test]/test.cpp
+++ [Test]/test.cpp
@@ -1,26 +1,10 @@
-//#include"Config.hpp"
-#include"Architecture.hpp"
+#include<Config.hpp>
+#include<GlobalConfig.hpp>
 
 using namespace std;
-
-#include <iostream>
-#include<vector>
-#include<map>
-using namespace std;
+using namespace Convention;
 
 int main()
 {
-	double t = 0;
-	for (int i = 1; i < 80; i++)
-	{
-		t += 1.0 / (double)i;
-	}
-	double k = 0;
-	for (int i = 1; i < 80; i++)
-	{
-		k += 1.0 / (double)i;
-		cout << i << ":\t" << k << "/" << t << "\t=\t" << k / t * 100 << "% current step:\t" <<
-			1.0 / (double)i << endl;
-	}
-	return 0;
+	cout << ProjectConfig().LoadProperties().FindItem<std::string>("test", "test") << endl;
 }
